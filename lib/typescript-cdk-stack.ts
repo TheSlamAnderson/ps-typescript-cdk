62a6c95c2b0b51e7ef9c99e7723d723a17252554
Chris Anderson <Chris.Anderson@vynecorp.com>
2023-01-09 12:56:29 -0500

    Updating to build S3 Document bucket

---
+++ lib/typescript-cdk-stack.ts
@@ -0,0 +1,12 @@
+import * as cdk from '@aws-cdk/core';
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
+
+export class TypescriptCdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = new Bucket(this, 'DocumentsBucket', {
+      encryption: BucketEncryption.S3_MANAGED,
+    });
+  }
+}
